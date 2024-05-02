00037c61cba8d86d48a158309bc10333a0042a95
Đức Hoàng <duchoang206h@gmail.com>
2024-05-02 21:12:40 +0700

    feat: add player interface

---
+++ src/interfaces/user.ts
@@ -0,0 +1,12 @@
+import { ICharacter } from './character'
+
+export interface IUser {
+  id: string
+  email: string
+  fullname: string
+  avatar: string
+  role: string
+  online: boolean
+  password: string
+  character: string | ICharacter
+}
