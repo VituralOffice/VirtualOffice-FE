5fe510ecf8bd261f3228fc5056f34ddc436e01de
Đức Hoàng <duchoang206h@gmail.com>
2024-06-26 00:07:18 +0700

    default peerjs option

--- src/constant.ts
+++ src/constant.ts
@@ -1,8 +1,8 @@
 export const API_URL = import.meta.env.VITE_API_URL
 export const PEER_HOST = import.meta.env.VITE_PEER_HOST
 export const PEER_CONNECT_OPTIONS = {
-  host: PEER_HOST,
-  secure: true,
+  //host: PEER_HOST,
+  //secure: true,
 }
 export const UNKNOWN_ERROR = `An error ocurred`
 export const SENDTRY_DSN = import.meta.env.VITE_SENDTRY_DSN
