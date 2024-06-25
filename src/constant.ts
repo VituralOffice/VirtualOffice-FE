4ecca64ab2ab49b4c547a36d69358725ca788de1
Đức Hoàng <duchoang206h@gmail.com>
2024-06-26 00:19:00 +0700

    add stun config

--- src/constant.ts
+++ src/constant.ts
@@ -1,8 +1,19 @@
 export const API_URL = import.meta.env.VITE_API_URL
 export const PEER_HOST = import.meta.env.VITE_PEER_HOST
 export const PEER_CONNECT_OPTIONS = {
-  //host: PEER_HOST,
-  //secure: true,
+  host: PEER_HOST,
+  secure: true,
+  config: {
+    iceServers: [
+      { urls: 'stun:stun.l.google.com:19302' }, // Example STUN server
+      { urls: 'stun:stun1.l.google.com:19302' },
+      { urls: 'stun:stun2.l.google.com:19302' },
+      { urls: 'stun:stun3.l.google.com:19302' },
+      { urls: 'stun:stun4.l.google.com:19302' },
+      { urls: 'stun:global.stun.twilio.com:3478?transport=udp' },
+      { urls: 'stun:stun.services.mozilla.com' },
+    ],
+  },
 }
 export const UNKNOWN_ERROR = `An error ocurred`
 export const SENDTRY_DSN = import.meta.env.VITE_SENDTRY_DSN
