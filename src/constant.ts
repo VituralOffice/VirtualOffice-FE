6b16c7c1aaee60d8afaeb174e0f4e87e708b4cb2
Đức Hoàng <duchoang206h@gmail.com>
2024-06-26 23:19:31 +0700

    revert stun config

--- src/constant.ts
+++ src/constant.ts
@@ -14,31 +14,28 @@ export const PEER_CONNECT_OPTIONS = {
     // ],
     iceServers: [
       {
-        urls: 'stun:14.225.253.168:3478',
+        urls: 'stun:stun.relay.metered.ca:80',
       },
-      // {
-      //   urls: 'stun:stun.relay.metered.ca:80',
-      // },
-      // {
-      //   urls: 'turn:asia-east.relay.metered.ca:80',
-      //   username: 'ea5efd3c89635bc876b80bff',
-      //   credential: 'ObPpozbgAaGWMDPG',
-      // },
-      // {
-      //   urls: 'turn:asia-east.relay.metered.ca:80?transport=tcp',
-      //   username: 'ea5efd3c89635bc876b80bff',
-      //   credential: 'ObPpozbgAaGWMDPG',
-      // },
-      // {
-      //   urls: 'turn:asia-east.relay.metered.ca:443',
-      //   username: 'ea5efd3c89635bc876b80bff',
-      //   credential: 'ObPpozbgAaGWMDPG',
-      // },
-      // {
-      //   urls: 'turns:asia-east.relay.metered.ca:443?transport=tcp',
-      //   username: 'ea5efd3c89635bc876b80bff',
-      //   credential: 'ObPpozbgAaGWMDPG',
-      // },
+      {
+        urls: 'turn:asia-east.relay.metered.ca:80',
+        username: 'ea5efd3c89635bc876b80bff',
+        credential: 'ObPpozbgAaGWMDPG',
+      },
+      {
+        urls: 'turn:asia-east.relay.metered.ca:80?transport=tcp',
+        username: 'ea5efd3c89635bc876b80bff',
+        credential: 'ObPpozbgAaGWMDPG',
+      },
+      {
+        urls: 'turn:asia-east.relay.metered.ca:443',
+        username: 'ea5efd3c89635bc876b80bff',
+        credential: 'ObPpozbgAaGWMDPG',
+      },
+      {
+        urls: 'turns:asia-east.relay.metered.ca:443?transport=tcp',
+        username: 'ea5efd3c89635bc876b80bff',
+        credential: 'ObPpozbgAaGWMDPG',
+      },
     ],
   },
 }
